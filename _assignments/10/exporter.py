ff1b3873126708c300aebf8cdb6abbc32ee2c631
soonwook34 <soonwook34@gmail.com>
2020-09-05 13:30:15 +0900

    remove .csv and make prettier form on saveToFile(exporter.py)

--- _assignments/10/exporter.py
+++ _assignments/10/exporter.py
@@ -4,7 +4,7 @@ def saveToFile(term, jobs):
     file = open(f"{term}-jobs.csv", mode="w")
     writer = csv.writer(file)
 
-    writer.writerow(["title", "company", "link"])
+    writer.writerow(["TITLE", "COMPANY", "LINK"])
     for job in jobs:
         writer.writerow(list(job.values()))
 
